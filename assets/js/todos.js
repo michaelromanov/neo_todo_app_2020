94dc9b7b67045dffae47cc4b768137e0b97dd0c1
michaelromanov <michael.alexander.romanov@gmail.com>
2020-04-19 16:32:15 -0400

    upd

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,19 +2,27 @@
 
 // Check Off Specific Todos By Clicking:
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
-})
+});
 
 //Click on X to delete Todo:
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
    $(this).parent().fadeOut(500, function(){
      $(this).remove();
    });
-   event.stopPropogation();
-})
+   event.stopPropagation();
+});
 
-
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    //grabbing a new todo from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+  }
+});
 
 
 
