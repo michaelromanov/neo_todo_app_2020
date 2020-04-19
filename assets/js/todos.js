abb694668323188c8112ebabd63451137b8cd01a
michaelromanov <michael.alexander.romanov@gmail.com>
2020-04-19 02:22:13 -0400

    upd

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,2 +1,55 @@
 // alert("JS Connected!")
 
+// Check Off Specific Todos By Clicking
+
+$("li").click(function(){
+  $(this).toggleClass("completed");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $("li").click(function(){
+//   $(this).css("color", "gray");
+//   $(this).css("text-decoration", "line-through");
+// })
+// $("li").click(function(){
+//   //if li gray
+//     //turn it black
+//   //else 
+//     //turn it gray
+
+//     if($(this).css("color") === "rgb(128, 128, 128)")  {
+//       $(this).css({
+//         color: "black",
+//         textDecoration: "none"
+//       }); 
+//     } else {
+//       $(this).css({
+//         color: "gray",
+//         textDecoration: "line-through"
+//       }); 
+//     }
+// })
+
+
