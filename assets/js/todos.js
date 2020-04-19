456e598d51e0cbf7b0a0fb6cf3759dba0c681151
michaelromanov <michael.alexander.romanov@gmail.com>
2020-04-19 03:54:19 -0400

    upd

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,18 @@
 // alert("JS Connected!")
 
-// Check Off Specific Todos By Clicking
+// Check Off Specific Todos By Clicking:
 
 $("li").click(function(){
   $(this).toggleClass("completed");
 })
 
-
+//Click on X to delete Todo:
+$("span").click(function(event){
+   $(this).parent().fadeOut(500, function(){
+     $(this).remove();
+   });
+   event.stopPropogation();
+})
 
 
 
